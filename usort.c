5d9a0ee7b3494189ea03b821dbce02145736578e
edarsh7 <edarsh7@gmail.com>
2021-01-19 11:59:12 -0800

    idk

--- usort.c
+++ usort.c
@@ -52,8 +52,8 @@ void multiProcessMergeSort(int arr[], int left, int right)
     case 0:
       r_array = (int *)shmat (shmid, (void*)0,0);
       int a_size = sizeof(r_array)/sizeof(r_array[0]);
-      printf("xr_arr[0]=%d\n",r_array[1]);
       singleProcessMergeSort(r_array, 0, a_size - 1);
+      printf("a[0]=%d, a[1]=%d, a[2]=%d \n",r_array[0],r_array[1],r_array[2]);
       shmdt(r_array);
       exit(0);
     default:
