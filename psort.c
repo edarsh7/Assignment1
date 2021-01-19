af55ccabc7ec61109b8f29cfa7c842482a7261dd
Edmond Arshakyan <earshaky@noggin.soe.ucsc.edu>
2021-01-18 18:08:19 -0800

    first commit

---
+++ psort.c
@@ -0,0 +1,36 @@
+/************************************************************************
+ * 
+ * CSE130 Winter 2021 Assignment 1
+ *  
+ * POSIX Shared Memory Multi-Process Merge Sort
+ * 
+ * Copyright (C) 2020-2021 David C. Harrison. All right reserved.
+ *
+ * You may not use, distribute, publish, or modify this code without 
+ * the express written permission of the copyright holder.
+ *
+ ************************************************************************/
+
+#include "merge.h"
+
+/* LEFT index and RIGHT index of the sub-array of ARR[] to be sorted */
+void singleProcessMergeSort(int arr[], int left, int right) 
+{
+  if (left < right) {
+    int middle = (left+right)/2;
+    singleProcessMergeSort(arr, left, middle); 
+    singleProcessMergeSort(arr, middle+1, right); 
+    merge(arr, left, middle, right); 
+  } 
+}
+
+/* 
+ * This function stub needs to be completed
+ */
+void multiProcessMergeSort(int arr[], int left, int right) 
+{
+  // Delete this line, it's only here to fail the code quality check
+  int i = 0;
+
+  // Your code goes here 
+}
