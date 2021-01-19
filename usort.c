fd14e982c647687d7639d0da5121aadfd1b2eb93
edarsh7 <edarsh7@gmail.com>
2021-01-19 01:45:50 -0800

    idk

--- usort.c
+++ usort.c
@@ -36,22 +36,31 @@ void singleProcessMergeSort(int arr[], int left, int right)
  */
 void multiProcessMergeSort(int arr[], int left, int right) 
 {
+  //we have an array
   int array[4] = {1,2,3,4};
   
+  //we create a shared memory segment that has an ID called shmid
   int shmid = shmget(IPC_PRIVATE, 1024, 0666|IPC_CREAT);
+  //we attach the address of the shared memory segment to an int pointer called array2
   int* array2 =  (int *)shmat (shmid, (void*)0,0);
-
+  //we copy the elements in the array into the shared memory space called array2
   memcpy(array2, array, sizeof(array));
   printf("parent process: %d has array[0] as %d\n", getpid(), array[0]);
 
+  //now we fork the process
   switch(fork()){
     case -1:
       exit;
+    // this is the child process that has array2, and array
     case 0:
       array2[0] = 9;
+      array[0] = 5;
       printf("child process: %d has array[0] as: %d\n\n", getpid(), array2[0]);
+      printf("C P array[0] = %d", array[0]);
       break;
     default:
+      array[0] = 4;
+      printf("P P array[0] = %d", array[0]);
       wait(NULL);
       shmdt(array2);
       shmctl(shmid, IPC_RMID,NULL);
